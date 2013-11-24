9e1982e6ac9f5d28fa498d00629b2a3a5c93e82f
Stanislav O Chernykh <stanislav.chernykh90@gmail.com>
2013-11-24 04:38:40 +0400

    initial commit

---
+++ app/scripts/controllers/My.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('todoApp')
+  .controller('MyctrlCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/directives/screenshot.js
@@ -0,0 +1,109 @@
+'use strict';
+
+angular.module('todoApp')
+    .directive('screenshot', function () {
+        return {
+            template:
+                '<img id="screenshot" class="vga" src="">'+
+                '<canvas style="display:none;" class="vga"></canvas>',
+            restrict: 'E',
+            controller : function( $scope, $element, $attrs, $transclude) {
+
+
+                var videoElement = document.querySelector("video");
+                var videoSelect = document.querySelector("select#videoSource");
+                var canvas = document.querySelector('canvas');
+                var ctx = canvas.getContext('2d');
+                var width = 320,
+                    height = 0;
+
+                navigator.getUserMedia = navigator.getUserMedia ||
+                    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                function snapshot() {
+                    if (window.stream) {
+
+                        canvas.width = width;
+                        canvas.height = height;
+                        ctx.drawImage(videoElement, 0, 0,width,height);
+                        // "image/webp" works in Chrome.
+                        // Other browsers will fall back to image/png.
+                        document.querySelector('#screenshot').src = canvas.toDataURL('image/webp');
+                    }
+                }
+
+                videoElement.addEventListener('click', snapshot, false);
+
+                function gotSources(sourceInfos) {
+                    for (var i = 0; i != sourceInfos.length; ++i) {
+                        var sourceInfo = sourceInfos[i];
+                        var option = document.createElement("option");
+                        option.value = sourceInfo.id;
+                        if (sourceInfo.kind === 'video') {
+                            option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
+                            videoSelect.appendChild(option);
+                        } else {
+                            console.log('Some other kind of source: ', sourceInfo);
+                        }
+                    }
+                }
+
+                if (typeof MediaStreamTrack === 'undefined'){
+                    alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
+                } else {
+                    MediaStreamTrack.getSources(gotSources);
+                }
+
+
+                function successCallback(stream) {
+                    
+                    window.stream = stream; // make stream available to console
+                    videoElement.src = window.URL.createObjectURL(stream);
+                    videoElement.play();
+
+                    height = videoElement.videoHeight / (videoElement.videoWidth/width);
+                    videoElement.setAttribute('width', width);
+                    videoElement.setAttribute('height', height);
+                    canvas.setAttribute('width', width);
+                    canvas.setAttribute('height', height);
+                }
+
+                function errorCallback(error){
+                    console.log("navigator.getUserMedia error: ", error);
+                }
+
+                this.start = function(){
+                    if (!!window.stream) {
+                        videoElement.src = null;
+                        window.stream.stop();
+                    }
+
+                    var videoSource = videoSelect.value;
+                    var constraints = {
+                        video: {
+                            mandatory : {
+                                maxHeight : 640,
+                                maxWidth : 480
+                            },
+                            optional: [{sourceId: videoSource}]
+                        }
+                    };
+                    navigator.getUserMedia(constraints, successCallback, errorCallback);
+                }
+
+                videoSelect.onchange = this.start;
+
+
+            },
+            link: function postLink(scope, element, attrs,controller) {
+
+
+                controller.start();
+
+
+            }
+        };
+    });
+
+
+
